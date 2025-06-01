a4b074d9fb00441e23404f37bf0de6e0bfe48ad3
saikumaryada <53844340+saiky-devops@users.noreply.github.com>
2025-06-01 11:31:31 -0700

    Remove sendgrid config

--- config.py
+++ config.py
@@ -17,9 +17,6 @@ EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")  # Use App Password for Gmail
 SMTP_SERVER = os.getenv("SMTP_SERVER", "smtp.gmail.com")
 SMTP_PORT = int(os.getenv("SMTP_PORT", "587"))
 
-# SendGrid Configuration (only needed if USE_SENDGRID is True)
-SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
-
 # Sources to scrape
 NEWS_SOURCES = [
     {"name": "DevOps.com", "url": "https://devops.com/category/blogs/"},
